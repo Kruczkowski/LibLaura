1d59d75ffa75398bdf375f6fcd47af73556525d3
Kruczkowski <pkruczkowski@wi.zut.edu.pl>
2016-05-20 08:38:31 +0200

    Cleanup

--- examples/GUI/CMakeFiles/LauraGUI_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "LauraGUI_automoc.cpp"
-  "CMakeFiles/LauraGUI_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/LauraGUI_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
